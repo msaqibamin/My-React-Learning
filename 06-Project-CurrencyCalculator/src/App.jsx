28eb80710baeeeb9a4cd3ddb79ad7dcd94a5e0ca
Muhammad Saqib Amin <mohdsaqib.amin@gmail.com>
2023-10-26 13:25:46 +0500

    Currency Calculator
    Currency Calculator

---
+++ 06-Project-CurrencyCalculator/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-gray-400 p-3 rounded-lg h-full justify-center mx-auto flex w-full max-w-lg">
+        <h1 className="text-3xl font-bold bg-blue-500 rounded-lg mx-auto text-center text-white p-3">
+          CURRENCY CALCULATOR
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
